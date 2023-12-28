743ac5eb67218c3be808602433152ede2080b7fe
mittalfb <77157308+mmittal2021@users.noreply.github.com>
2023-12-28 11:26:37 +0000

    Milestone 3 - Task 3

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,3 +1,4 @@
+randomly_chosen_word = "mango"
 
 # Step 1: Create a while loop and set the condition to True. Setting the condition to True ensures that the code runs continuously.
 
@@ -7,9 +8,23 @@ while True:
 
     # Step 3: Check that the guess is a single alphabetical character.
     if len(guess) == 1 and guess.isalpha():
+        
+        # Step 1: Create an if statement that checks if the guess is in the word
+        if guess in randomly_chosen_word:
+            
+            # Step 2: Print "Good guess", if the letter is in the randomly selected word
+            print(f"Good guess! '{guess}' is in the word.")
+        else:
+            
+            # Step 3: If not true, print "Sorry guess is not in word"
+            print(f"Sorry, '{guess}' is not in the word. Try again.")
         # Step 4:If the guess passes the checks, break out of the loop.
         break
+    
     else:
         # Step 5: If the guess does not pass the checks, then print a message saying "Invalid letter. Please, enter a single alphabetical character."
         print("Invalid letter. Please, enter a single alphabetical character.")
+        
+
+
 
