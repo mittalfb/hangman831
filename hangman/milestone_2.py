af2dc3786fcefe29713f3aed3dac96ad6803b3b5
mittalfb <77157308+mmittal2021@users.noreply.github.com>
2023-12-28 11:07:35 +0000

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,11 +1,15 @@
 import random
 
+#TODO Task 1
+
 #Create a list containing the names of your 5 favorite fruits and assign this list to a variable called word_list.
 word_list = ['banana', 'peach', 'mango', 'kiwi', 'grapefruit']
 
 #Print out the newly created list to the standard output (screen).
 print(word_list)
 
+
+#TODO Task 2
 #Create the random.choice method and pass the word_list variable into the choice method.
 
 def choice(wor_list):
@@ -16,6 +20,12 @@ def choice(wor_list):
     print(word)
 
 choice(word_list)
+
+#TODO Task 3
+
+#Using the input function, ask the user to enter a single letter.
+
+guess = input("Enter a single letter")
 
 
     
