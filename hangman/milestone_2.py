b45b42b2ec2c22c59fae528bd8a1494e1455e8a1
mittalfb <77157308+mmittal2021@users.noreply.github.com>
2023-12-28 11:15:09 +0000

    Milestone 2 - Task 4

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -2,30 +2,37 @@ import random
 
 #TODO Task 1
 
-#Create a list containing the names of your 5 favorite fruits and assign this list to a variable called word_list.
+# Step 1: Create a list containing the names of your 5 favorite fruits and assign this list to a variable called word_list.
 word_list = ['banana', 'peach', 'mango', 'kiwi', 'grapefruit']
 
-#Print out the newly created list to the standard output (screen).
+# Step 2: Print out the newly created list to the standard output (screen).
 print(word_list)
 
 
-#TODO Task 2
-#Create the random.choice method and pass the word_list variable into the choice method.
+# TODO Task 2
+# Step 1: Create the random.choice method and pass the word_list variable into the choice method.
 
 def choice(wor_list):
-    #Assign the randomly generated word to a variable called word.
+    #Step 2: Assign the randomly generated word to a variable called word.
     word = random.choice(word_list)
     
-    #Print out word to the standard output. 
+    #Step 3: Print out word to the standard output. 
     print(word)
 
 choice(word_list)
 
-#TODO Task 3
+# TODO Task 3
 
 #Using the input function, ask the user to enter a single letter.
 
 guess = input("Enter a single letter")
 
+# TODO Task 4
 
-    
+# Step 1: Create an if statement that checks if the length of the input is equal to 1 and the input is alphabetical.
+if len(guess) == 1 and guess.isalpha():
+    # Step 2: Print a message for a valid input
+    print("Good guess!")
+else:
+    # Step 3: Print a message for an invalid input
+    print("Oops! That is not a valid input.")
